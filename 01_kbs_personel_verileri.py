cb2e3fd92f0764ab8be5f10d7d694a06d3412331
Yahya YILDIRIM <yahya.yldrm@gmail.com>
2022-12-07 11:13:40 +0300

    ikysden indirilen rapor, önce html olarak okundu, sonra excele dataframe içine eklendi.

--- 01_kbs_personel_verileri.py
+++ 01_kbs_personel_verileri.py
@@ -1,8 +1,5 @@
 #!/usr/bin/env python3
-# Modülleri yüklemek için
-# sudo python3 -m pip install -U pandas==1.3.5
-# sudo python3 -m pip install -U numexpr==2.8.4
-# sudo python3 -m pip install -U numpy==1.21.6
+
 import sys
 sys.dont_write_bytecode = True
 
@@ -13,7 +10,7 @@ import glob
 
 
 def kbs_personel_verileri():
-	dosya = glob.glob('./kbs/MemurRaporlar_PersonelBilgileri.xls', recursive = True)
+	dosya = glob.glob('./kbs/_reports_MemurRaporlar_PersonelBilgileri_jrxml*.xls', recursive = True)
 	for file in dosya:
 		df = pd.DataFrame(pd.read_excel(file, sheet_name=0, skiprows=15))
 	df.dropna(axis=0, thresh=8, inplace=True)			
