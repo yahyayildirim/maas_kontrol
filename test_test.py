ac4358b5b798682a8e454789ca142c9d6b1c91ff
Yahya YILDIRIM <yahya.yldrm@gmail.com>
2022-12-11 22:57:13 +0300

    kodlarda ekleme ve güncelleme yapıldı.

--- test_test.py
+++ test_test.py
@@ -9,8 +9,8 @@ import xlrd
 
 
 #Excel Dosyamızı çağırıyoruz, ilk iki satırı almıyoruz ve veri değişkenine atıyoruz
-kbss_veriseti = pd.DataFrame(pd.read_excel('./rapor/kbss_yeni.ods', sheet_name=0))
-ikys_veriseti = pd.DataFrame(pd.read_excel('./rapor/ikys_yeni.ods', sheet_name=0))
+kbss_veriseti = pd.DataFrame(pd.read_excel('./rapor/ikys_personel_verileri.ods', sheet_name=0))
+ikys_veriseti = pd.DataFrame(pd.read_excel('./rapor/kbs_bordro_verileri.ods', sheet_name=0))
 
 #print(kbss_veriseti.shape)
 #print(ikys_veriseti.shape)
@@ -39,8 +39,31 @@ kbss_derece_kademe = kbss_veriseti["Derece"]
 #Sicil sutunundaki verileri listeye alıyoruz.
 #print(veri["Sicil"].tolist())
 
-bir = pd.concat([kbss_veriseti, ikys_veriseti], axis=1)
-bir.to_excel('birlestirildi.ods', index=False)
+df3 = pd.merge(kbss_veriseti, ikys_veriseti, how='outer', indicator='Exist')
+df3 = df3.loc[df3['Exist'] != 'both']
+df3.sort_values(by=['TC Kimlik'], inplace=True)
+df3.to_excel('merge_rapor.ods', index=False)
+
+#df_diff = pd.concat([df1,df2]).drop_duplicates(keep=False)
+bir = pd.concat([kbss_veriseti, ikys_veriseti]).drop_duplicates(keep=False)
+bir.sort_values(by=['TC Kimlik'], inplace=True)
+bir.to_excel('concat_rapor.ods', index=False)
+
+
+
+df_all = pd.concat([kbss_veriseti.set_index('TC Kimlik'), ikys_veriseti.set_index('TC Kimlik')], axis='columns', keys=['KBS', 'İKYS'])
+df_final = df_all.swaplevel(axis='columns')[kbss_veriseti.columns[1:]]
+
+def highlight_diff(data, color='yellow'):
+    attr = 'background-color: {}'.format(color)
+    other = data.xs('KBS', axis='columns', level=-1)
+    return pd.DataFrame(np.where(data.ne(other, level=0), attr, ''), index=data.index, columns=data.columns)
+
+df_final.style.apply(highlight_diff, axis=None)
+df_final.to_excel('fark_rapor.ods')
+
+
+
 
 #Hizmet sınıfı ve unvana göre grupluyoruz ve for döngüsü ile listeye alıyoruz.
 #for name, sinif in df.groupby(["Unvan", "Sınıf"]):
