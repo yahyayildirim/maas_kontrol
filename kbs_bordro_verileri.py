8e21573c54daabbe5be604ea7b9fa851eaf3d6c4
Yahya YILDIRIM <yahya.yldrm@gmail.com>
2022-12-24 22:48:43 +0300

    bordro özet detaylı sayfasında verileri okuma eklendi.

--- kbs_bordro_verileri.py
+++ kbs_bordro_verileri.py
@@ -12,12 +12,36 @@ import time
 from glob import glob
 import sabitler
 
-def kbs_bordro_verileri():
+def kbs_temiz_veri():
+	#Excel Dosyamızı çağırıyoruz, ilk iki satırı almıyoruz ve veri değişkenine atıyoruz
+
 	dosya = glob('./kbs/BordroDokumu*', recursive=False)
-	data = [pd.read_excel(f, sheet_name=0, skiprows=1) for f in dosya]
+	data = [pd.read_excel(f, sheet_name=1, skiprows=6) for f in dosya]
 	df = pd.DataFrame(data[0])
-	
-	df = df.dropna(how='all', axis=1)
+
+	#df = pd.DataFrame(pd.read_excel('./kbs/BordroDokumu_ortahisar.xlsx', sheet_name=1, skiprows=6))
+	df.dropna(axis=0, how='any', inplace=True)
+	df.dropna(axis=1, thresh=2, inplace=True)
+	say = 0
+	df_list = []
+	for c in range(len(df.columns)):
+		col = df.columns[say].split('\n')
+		sutun = []
+		for cl in col:
+			sutun.append(cl.strip())
+		df_say = df[df.columns[c]].str.split('\n', expand=True)
+		df_list.append(pd.DataFrame(df_say.values, columns=sutun))
+		say = say + 1
+	deneme = pd.concat(df_list, axis=1)
+	deneme.to_excel('./kbs/Bordro_Dokumu_Temiz.xlsx', index=False)
+
+
+def kbs_bordro_verileri():
+	dosya = glob('./kbs/Bordro_Dokumu*', recursive=False)
+	data = [pd.read_excel(f) for f in dosya]
+	df = pd.DataFrame(data[0])
+
+	#df = df.dropna(how='all', axis=1)
 	if 'Ek Tazminat' in df.columns:
 		df['Ek Tazminat'] = df['Ek Tazminat']
 	else:
@@ -37,15 +61,18 @@ def kbs_bordro_verileri():
 	df[['Personel No', 'TC Kimlik']] = df['Personel No TC.Kimlik No'].str.split('-', expand=True)
 
 	# Hizmet Sın.-Ünvan sütununu Sınıf ve Unvan olarak iki sutüna ayırıyoruz.
-	df['Hizmet Sın.-Ünvan'].replace(regex=True, inplace=True, to_replace=r'-Hat.', value=r'.Hat.')
+	#df['Hizmet Sın.-Ünvan'].replace(regex=True, inplace=True, to_replace=r'-Hat.', value=r'.Hat.')
 	
-	df[['Sınıf', 'Unvan']] = df['Hizmet Sın.-Ünvan'].str.split('-', expand=True)
+	df[['Sınıf', 'Unvan']] = df['Hizmet Sın.-Ünvan'].str.split('-', n=1, expand=True)
 
 	# Öd.Es.D.-K. Em.Es.D.-K. sütununu 'Derece-Kademe' ve 'Yan Ödeme' olarak iki sutüna ayırıyoruz.
-	df[['Derece-Kademe', 'Yan Ödeme']] = df['Öd.Es.D.-K. Em.Es.D.-K.'].str.split(' ', expand=True)
+	df[['Derece-Kademe', 'Yan Ödeme']] = df['Öd.Es.D.-K. Em.Es.D.-K.'].str.split(' ', n=1, expand=True)
 
 	# Derece-Kademe sütununu 'Derece' ve 'Kademe' olarak iki sutüna ayırıyoruz.
-	df[['Derece', 'Kademe']] = df['Derece-Kademe'].str.split('-', expand=True)
+	df[['Derece', 'Kademe']] = df['Derece-Kademe'].str.split('-', expand=True).apply(pd.to_numeric).fillna(0)
+
+	# derece ve kademeyi alarak gösterge puanını maas_verileri.xlsx dosyasından çekiyoruz.
+	df['Yan Ödeme'] = df.apply(lambda row: sabitler.gosterge_puani(row["Derece"], row["Kademe"]), axis=1)
 
 	# Öd.Ekgös-Em.Ekgös sütununu 'Ek Gösterge' ve 'Em-EkGösterge' olarak iki sutüna ayırıyoruz.
 	df[['Ek Gösterge', 'Em-EkGösterge']] = df['Öd.Ekgös-Em.Ekgös'].str.split('-', expand=True)
@@ -53,7 +80,7 @@ def kbs_bordro_verileri():
 	# Kıdem Ay-Kıdem Yıl sütununu 'Hizmet Süresi (Ay)' ve 'Hizmet Süresi (Yıl)' olarak iki sutüna ayırıyoruz.
 	df[['Hizmet Süresi (Ay)', 'Hizmet Süresi (Yıl)']] = df['Kıdem Ay-Kıdem Yıl'].str.split('-', expand=True)
 
-	df[['TC Kimlik', 'Derece', 'Kademe', 'Yan Ödeme', 'Ek Gösterge', 'Hizmet Süresi (Yıl)']] = df[['TC Kimlik', 'Derece', 'Kademe', 'Yan Ödeme', 'Ek Gösterge', 'Hizmet Süresi (Yıl)']].apply(pd.to_numeric).fillna(0)		
+	df[['TC Kimlik', 'Derece', 'Kademe', 'Yan Ödeme', 'Ek Gösterge', 'Hizmet Süresi (Yıl)']] = df[['TC Kimlik', 'Derece', 'Kademe', 'Yan Ödeme', 'Ek Gösterge', 'Hizmet Süresi (Yıl)']].apply(pd.to_numeric).fillna(0)
 
 	#df = df.drop_duplicates(subset=['TC Kimlik'], ignore_index=True, keep=False, inplace=False)
 	df = df.sort_values('Net Ödenen', ascending=False).drop_duplicates('TC Kimlik').sort_index()
@@ -82,8 +109,9 @@ def kbs_bordro_verileri():
 	df.sort_values(by=['Adı Soyadı'], inplace=True, ignore_index=True)
 
 	df.to_excel('./kbs/kbs_bordro_verileri.xlsx', index=False, freeze_panes=(1,2))
-	print('2. KBS personel bordro bilgileri uygun formata getirildi...')
+	print('1. KBS personel bordro bilgileri uygun formata getirildi...')
 	time.sleep(2)
 
 if __name__ == '__main__':
+	kbs_temiz_veri()
 	kbs_bordro_verileri()
