4f31e91b58f152c344273da680a5c37b2fb6cda8
Yahya YILDIRIM <yahya.yldrm@gmail.com>
2022-12-31 17:09:45 +0300

    time.sleep() şeysi kaldırıldı.

--- verileri_raporla.py
+++ verileri_raporla.py
@@ -11,22 +11,21 @@ import time
 kbs_verisi = pd.DataFrame(pd.read_excel('./kbs/kbs_bordro_verileri.xlsx'))
 ikys_verisi = pd.DataFrame(pd.read_excel('./ikys/ikys_personel_verileri.xlsx'))
 print('%40')
-time.sleep(1)
+
 # KBS Sisteminde bilgisi bulunup, İKYS Sisteminde kaydı bulunmayan Personel
 kbs_in_ikys_out = kbs_verisi[~kbs_verisi['TC Kimlik'].isin(ikys_verisi['TC Kimlik'])]
 kbs_in_ikys_out.to_excel('./rapor/kbsde_olup_ikysde_olmayanlar.xlsx', index=False, header=True, freeze_panes=(1,0))
 print('%50')
-time.sleep(1)
+
 kbs_ikys_esitle = kbs_verisi.loc[kbs_verisi['TC Kimlik'].isin(ikys_verisi['TC Kimlik'])]
 kbs_ikys_esitle.to_excel('./kbs/kbs_bordro_verileri.xlsx', index=False, header=True, freeze_panes=(1,0))
 print('%60')
-time.sleep(1)
+
 # İKS Sisteminde bilgisi bulunup, KBS Sisteminde kaydı bulunmayan Personel
 ikys_in_kbs_out = ikys_verisi[~ikys_verisi['TC Kimlik'].isin(kbs_verisi['TC Kimlik'])]
 ikys_in_kbs_out.to_excel('./rapor/ikysde_olup_kbsde_olmayanlar.xlsx', index=False, header=True, freeze_panes=(1,0))
 print('%70')
-time.sleep(1)
+
 ikys_kbs_esitle = ikys_verisi.loc[ikys_verisi['TC Kimlik'].isin(kbs_verisi['TC Kimlik'])]
 ikys_kbs_esitle.to_excel('./ikys/ikys_personel_verileri.xlsx', index=False, header=True, freeze_panes=(1,0))
 print('%80')
-time.sleep(1)
