d77278e501b446d2dfdfbffdb464f5a0095cf8cd
Yahya YILDIRIM <yahya.yldrm@gmail.com>
2023-02-13 15:11:30 +0300

    Albaraka Türk için banka listesi hazırlama eklendi.

--- banka/banka_listesi_olustur.py
+++ banka/banka_listesi_olustur.py
@@ -7,6 +7,9 @@ from datetime import datetime
 import openpyxl
 from openpyxl.utils.dataframe import dataframe_to_rows
 
+import locale
+locale.setlocale(locale.LC_ALL, 'tr_TR.UTF-8')
+
 # Taslak Excel Dosyamız
 excel_dosyasi = openpyxl.load_workbook('OrnekMaasOdemeDosyasi.xlsx')
 
@@ -15,6 +18,9 @@ excel_sayfasi = excel_dosyasi['Sheet1']
 
 bugun = datetime.today()
 tarih = datetime.strftime(bugun, '%d%m%Y_%H%M%S')
+
+bu_yil = str(datetime.now().year)
+bu_ay = str(datetime.now().strftime("%B")).upper()
 
 def bankaListesi():
 	#İKYSden indirdiğimiz dosya, html formatında olduğu için önce read_html metodu ile açıp, xlsx formatında tekrar kaydediyoruz.
@@ -33,7 +39,8 @@ def bankaListesi():
 			df = pd.DataFrame(dfs.values, columns=sutun_adi)
 			df['MAAŞ TUTARI'] = df['MAAŞ TUTARI'].str.replace(".", "", regex=False)
 			df['MAAŞ TUTARI'] = df['MAAŞ TUTARI'].str.replace(",", ".", regex=False).astype(float)
-			#df[['SIRA NO', 'TC KIMLIK NO', 'BANKA HESAP NO']] = df[['SIRA NO', 'TC KIMLIK NO', 'BANKA HESAP NO']].apply(pd.to_numeric)
+			#aciklama = input(f"Lütfen {rapor} dosyası için açıklama giriniz: ")
+			df['AÇIKLAMA'] = "SÖZ. MAAŞ ÖDEMESİ"
 			df_list.append(df)
 
 		elif 'MemurRaporlar_BankaListesi' in rapor:
@@ -47,7 +54,8 @@ def bankaListesi():
 			df = pd.DataFrame(dfs.values, columns=sutun_adi)
 			df['MAAŞ TUTARI'] = df['MAAŞ TUTARI'].str.replace(".", "", regex=False)
 			df['MAAŞ TUTARI'] = df['MAAŞ TUTARI'].str.replace(",", ".", regex=False).astype(float)
-			#df[['SIRA NO', 'TC KIMLIK NO', 'BANKA HESAP NO']] = df[['SIRA NO', 'TC KIMLIK NO', 'BANKA HESAP NO']].apply(pd.to_numeric)
+			#aciklama = input(f"Lütfen {rapor} dosyası için açıklama giriniz: ")
+			df['AÇIKLAMA'] ="KAD. MAAŞ ÖDEMESİ"
 			df_list.append(df)
 
 		elif 'edBankaListe' in rapor:
@@ -57,31 +65,35 @@ def bankaListesi():
 			df = pd.DataFrame(dfs.values, columns=sutun_adi)
 			df['IBAN NO'] = df['BANKA HESAP NO'].values
 			df['ADI SOYADI'] = df['ADI SOYADI'].str.title()
+			#aciklama = input(f"Lütfen {rapor} dosyası için açıklama giriniz: ")
+			df['AÇIKLAMA'] = "EK-DERS"
 			df_list.append(df)
 
 		else:
-			sutun_ad = ['SIRA NO', 'TC KIMLIK NO', 'ADI SOYADI', 'IBAN NO', 'MAAŞ TUTARI']
+			sutun = ['SIRA NO', 'TC KIMLIK NO', 'ADI SOYADI', 'IBAN NO', 'AGİ', 'ÜCRET', 'MAAŞ TUTARI']
 			dfs = pd.read_excel(rapor, skiprows=10)
 			dfs.dropna(axis=0, how='all', inplace=True)
 			dfs.dropna(axis=0, thresh=5, inplace=True)
 			dfs.dropna(axis=1, how='all', inplace=True)
+			sutun_ad = dfs.columns
+			#print(dfs)
 			df = pd.DataFrame(dfs.values, columns=sutun_ad)
+			if 'Unvan' in df.columns:
+				df.drop(['Unvan'], axis=1, inplace=True)
+			df.columns = sutun
+			#df = df.set_axis(sutun_ad, axis=1, inplace=False)
 			df['ADI SOYADI'] = df['ADI SOYADI'].str.title()
-			df['BANKA HESAP NO'] = ""
-			df['UNVANI'] = ""
+			#aciklama = input(f"Lütfen {rapor} dosyası için açıklama giriniz: ")
+			df['AÇIKLAMA'] = "GEÇİCİ EK-DERS"
 			df_list.append(df)
 
 	df = pd.concat(df_list, axis=0)
 	df['ŞUBE KODU'] = ""
 	df['HESAP NUMARASI'] = ""
 	df['EK HESAP NUMARASI'] = ""
-	df['AÇIKLAMA'] = "MARDİN MÜF. MAAŞ/EKDERS ÖDEMESİ"
-
 	df = df[['ADI SOYADI', 'IBAN NO', 'ŞUBE KODU', 'HESAP NUMARASI', 'EK HESAP NUMARASI', 'AÇIKLAMA', 'MAAŞ TUTARI']]
-
 	for veri in dataframe_to_rows(df, index=False, header=False):
 		excel_sayfasi.append(veri)
-
 	#OrnekMaasOdemeDosyasi.xlsx dosyasını banka_listesi_xxxxxxxx_xxxxxx.xlsx olarak kaydediyoruz.
 	excel_dosyasi.save('banka_listesi_' + tarih + '.xlsx')
 
