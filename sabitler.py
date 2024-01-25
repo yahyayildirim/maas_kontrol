91956ce14b70067804374151ea2730185989e4a7
Yahya YILDIRIM <yahya.yldrm@gmail.com>
2024-01-25 17:00:08 +0300

    Vekil personel ile ilgili ocak ayı katsayı hatası giderildi.

--- sabitler.py
+++ sabitler.py
@@ -3,7 +3,10 @@
 import os
 import sys
 import pandas as pd
+from datetime import datetime
 sys.dont_write_bytecode = True
+
+bu_ay = datetime.now().strftime("%B")
 
 # maas_verileri.xlsx dosyasında bulunan herbir sayfayı ayrı ayrı DataFramelere aktardık.
 bu_dizin = os.path.dirname(__file__) + '/'
@@ -23,23 +26,47 @@ def aylik_katsayi(gosterge_puani, unvan, izin):
 	if izin == "Aylıksız İzin" or unvan == "İl Müftüsü":
 		aylik_tutar = 0
 	elif unvan in vekilper:
-		aylik_tutar = (df_1['aylik_katsayi'].iloc[-1] * gosterge_puani / 3) * 2
+		if bu_ay == "Ocak":
+			aylik_tutar = (df_1['aylik_katsayi'].iloc[-2] * gosterge_puani / 3) * 2
+		else:
+			aylik_tutar = (df_1['aylik_katsayi'].iloc[-1] * gosterge_puani / 3) * 2
 	else:
 		aylik_tutar = df_1['aylik_katsayi'].iloc[-1] * gosterge_puani
 	return aylik_tutar
-	print(aylik_tutar)
+
 	# gosterge_puani x aylik_kaysayi = Aylık Tutar
 
-def tutardan_yan_odeme_puani_bul(tutar):
+def tutardan_yan_odeme_puani_bul(tutar, tabanaylik_tutar):
 	# yan ödeme aylık tutar / yan_odeme_katsayi
-	return tutar / df_1['yan_odeme_katsayi'].iloc[-1]
+	yeni_katsayi = round(df_1['taban_aylik_katsayi'].iloc[-1] * 1000 / 3 * 2, 2)
+	eski_katsayi = round(df_1['taban_aylik_katsayi'].iloc[-2] * 1000 / 3 * 2, 2)
+
+	if eski_katsayi == tabanaylik_tutar:
+		oran = tutar / df_1['yan_odeme_katsayi'].iloc[-2]
+
+	elif yeni_katsayi == tabanaylik_tutar:
+		oran = tutar / df_1['yan_odeme_katsayi'].iloc[-2]
+
+	else:
+		oran = tutar / df_1['yan_odeme_katsayi'].iloc[-1]
+
+	return oran
 
 def yan_odeme(gosterge_puani, unvan):
 	if unvan == "İl Müftüsü":
 		gosterge_puani = 0
-	#print(unvan, gosterge_puani)
-	return df_1['yan_odeme_katsayi'].iloc[-1] * gosterge_puani
-	# gosterge_puani x yan_odeme_katsayi
+
+	vekilper = ['Vekil M.K', 'Vekil İ-H']	
+	if unvan in vekilper:
+		if bu_ay == "Ocak":
+			yan_odeme_tutar = df_1['yan_odeme_katsayi'].iloc[-2] * gosterge_puani
+		else:
+			yan_odeme_tutar = df_1['yan_odeme_katsayi'].iloc[-2] * gosterge_puani
+	else:
+		yan_odeme_tutar = df_1['yan_odeme_katsayi'].iloc[-1] * gosterge_puani
+
+	return yan_odeme_tutar
+
 
 def taban_aylik(binlik, unvan):
 	if unvan == "İl Müftüsü":
@@ -54,7 +81,10 @@ def ek_gosterge(ek_gosterge, unvan, izin):
 	if izin == "Aylıksız İzin" or unvan == "İl Müftüsü":
 		return 0
 	elif unvan in vekilper:
-		ek_gosterge = ((df_1['aylik_katsayi'].iloc[-1] * ek_gosterge) / 3) * 2
+		if bu_ay == "Ocak":
+			ek_gosterge = ((df_1['aylik_katsayi'].iloc[-2] * ek_gosterge) / 3) * 2
+		else:
+			ek_gosterge = ((df_1['aylik_katsayi'].iloc[-1] * ek_gosterge) / 3) * 2
 		return ek_gosterge
 	else:
 		return df_1['aylik_katsayi'].iloc[-1] * ek_gosterge
@@ -91,8 +121,9 @@ def ozel_hizmet(unvan, derece, ogrenim, izin):
 	if izin == "Aylıksız İzin":
 		return 0
 	else:
-		unvanlar = ['İmam-Hat.', 'Müez.Kayyı', 'Kur.Krs.Öğ', 'Murakıp',  'Tekniker', 'Teknisyen', 'Vekil M.K', 'Vekil İ-H']
+		unvanlar = ['İmam-Hat.', 'Müez.Kayyı', 'Kur.Krs.Öğ', 'Murakıp',  'Tekniker', 'Teknisyen']
 		uz_unvanlar = ['Uzman Vaiz', 'Uz.İm.Hat', 'Kur.Uz.Öğ', 'Baş Müez.Kayyı', 'Baş Vaiz', 'Başimam', 'Kur.Baş.Öğ']
+		vekilper = ['Vekil M.K', 'Vekil İ-H']
 		#print(unvan, derece, ogrenim)
 
 		#Özel Hizmet Tazminatı = 9500 X Memur Maaş Katsayısı X Özel Hizmet Puanı / 100 formülüyle hesaplanır.
@@ -105,6 +136,13 @@ def ozel_hizmet(unvan, derece, ogrenim, izin):
 			ozel_hizmet_tutari = df_1['aylik_katsayi'].iloc[-1] * ozel_hizmet_taz * 9500 / 100
 		elif unvan == "İl Müftüsü":
 			ozel_hizmet_tutari = 0
+		elif unvan in vekilper:
+			if bu_ay == "Ocak":
+				ozel_hizmet_taz = df_3.loc[(df_3['unvan'] == unvan) & (df_3['derece'] == derece) & (df_3['ogrenim'] == ogrenim), 'oht_orani'].sum()
+				ozel_hizmet_tutari = df_1['aylik_katsayi'].iloc[-2] * ozel_hizmet_taz * 9500 / 100
+			else:
+				ozel_hizmet_taz = df_3.loc[(df_3['unvan'] == unvan) & (df_3['derece'] == derece) & (df_3['ogrenim'] == ogrenim), 'oht_orani'].sum()
+				ozel_hizmet_tutari = df_1['aylik_katsayi'].iloc[-1] * ozel_hizmet_taz * 9500 / 100				
 		else:
 			ozel_hizmet_taz = df_3.loc[(df_3['unvan'] == unvan) & (df_3['derece'] == derece), 'oht_orani'].sum()
 			ozel_hizmet_tutari = df_1['aylik_katsayi'].iloc[-1] * ozel_hizmet_taz * 9500 / 100
@@ -176,6 +214,7 @@ def yan_odeme_puani(unvan, derece, hizmetyili, izin):
 			is_riski = df_2.loc[df_2['unvan'] == unvan, 'is_riski'].sum()
 			tem_gucluk = df_2.loc[df_2['unvan'] == unvan, 'tem_gucluk'].sum()
 			mali_sorumluluk = df_2.loc[df_2['unvan'] == unvan, 'mali_sorumluluk'].sum()
+
 			return is_guclugu + is_riski + tem_gucluk + mali_sorumluluk
 
 # def yan_odeme_puani_kbs(tc):
@@ -189,9 +228,19 @@ def yan_odeme_puani(unvan, derece, hizmetyili, izin):
 # 	ozel_hizmet_orani = df_per.loc[df_per['TC Kimlik'] == tc, 'Özel Hizmet'].sum()
 # 	return ozel_hizmet_orani
 
-def tutardan_ozel_hizmet_orani_bul(tutar):
+def tutardan_ozel_hizmet_orani_bul(tutar, tabanaylik_tutar):
 	# Puan = Özel Hizmet Tazminatı Tutarı / (9500 X Memur Maaş Katsayısı) 100 formülüyle hesaplanır.
-	return tutar / (9500 * df_1['aylik_katsayi'].iloc[-1]) * 100
+	yeni_katsayi = round(df_1['taban_aylik_katsayi'].iloc[-1] * 1000 / 3 * 2, 2)
+	eski_katsayi = round(df_1['taban_aylik_katsayi'].iloc[-2] * 1000 / 3 * 2, 2)
+
+	if eski_katsayi == tabanaylik_tutar:
+		oran = tutar / (9500 * df_1['aylik_katsayi'].iloc[-2]) * 100
+	elif yeni_katsayi == tabanaylik_tutar:
+		oran = tutar / (9500 * df_1['aylik_katsayi'].iloc[-2]) * 100
+	else:
+		oran = tutar / (9500 * df_1['aylik_katsayi'].iloc[-1]) * 100
+
+	return oran
 
 def tutardan_666_orani_bul(tutar):
 	# Puan = Özel Hizmet Tazminatı Tutarı / (9500 X Memur Maaş Katsayısı) 100 formülüyle hesaplanır.
