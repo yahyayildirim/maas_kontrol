b39a11c8249ee3abeb522b226f15211ddfaabe56
Yahya YILDIRIM <yahya.yldrm@gmail.com>
2024-01-29 12:49:32 +0300

    try ve except fonksiyonları eklendi.

--- verileri_raporla.py
+++ verileri_raporla.py
@@ -14,25 +14,32 @@ from datetime import datetime
 bu_yil = datetime.now().year
 bu_ay = datetime.now().strftime("%B")
 
-# KBS ve İKYS verilerini DataFrame ekliyoruz.
-kbs_verisi = pd.DataFrame(pd.read_excel('./rapor/' + str(bu_yil) + '/' + str(bu_ay) + '/kbs_bordro_verileri.xlsx'))
-ikys_verisi = pd.DataFrame(pd.read_excel('./rapor/' + str(bu_yil) + '/' + str(bu_ay) + '/ikys_personel_verileri.xlsx'))
-print('%40')
+def raporla():
+	try:
+		# KBS ve İKYS verilerini DataFrame ekliyoruz.
+		kbs_verisi = pd.DataFrame(pd.read_excel('./rapor/' + str(bu_yil) + '/' + str(bu_ay) + '/kbs_bordro_verileri.xlsx'))
+		ikys_verisi = pd.DataFrame(pd.read_excel('./rapor/' + str(bu_yil) + '/' + str(bu_ay) + '/ikys_personel_verileri.xlsx'))
+		print('%40')
 
-# KBS Sisteminde bilgisi bulunup, İKYS Sisteminde kaydı bulunmayan Personel
-kbs_in_ikys_out = kbs_verisi[~kbs_verisi['TC Kimlik'].isin(ikys_verisi['TC Kimlik'])]
-kbs_in_ikys_out.to_excel('./rapor/' + str(bu_yil) + '/' + str(bu_ay) + '/kbsde_olup_ikysde_olmayanlar.xlsx', index=False, header=True, freeze_panes=(1,0))
-print('%50')
+		# KBS Sisteminde bilgisi bulunup, İKYS Sisteminde kaydı bulunmayan Personel
+		kbs_in_ikys_out = kbs_verisi[~kbs_verisi['TC Kimlik'].isin(ikys_verisi['TC Kimlik'])]
+		kbs_in_ikys_out.to_excel('./rapor/' + str(bu_yil) + '/' + str(bu_ay) + '/kbsde_olup_ikysde_olmayanlar.xlsx', index=False, header=True, freeze_panes=(1,0))
+		print('%50')
 
-kbs_ikys_esitle = kbs_verisi.loc[kbs_verisi['TC Kimlik'].isin(ikys_verisi['TC Kimlik'])]
-kbs_ikys_esitle.to_excel('./rapor/' + str(bu_yil) + '/' + str(bu_ay) + '/kbs_bordro_verileri.xlsx', index=False, header=True, freeze_panes=(1,0))
-print('%60')
+		kbs_ikys_esitle = kbs_verisi.loc[kbs_verisi['TC Kimlik'].isin(ikys_verisi['TC Kimlik'])]
+		kbs_ikys_esitle.to_excel('./rapor/' + str(bu_yil) + '/' + str(bu_ay) + '/kbs_bordro_verileri.xlsx', index=False, header=True, freeze_panes=(1,0))
+		print('%60')
 
-# İKS Sisteminde bilgisi bulunup, KBS Sisteminde kaydı bulunmayan Personel
-ikys_in_kbs_out = ikys_verisi[~ikys_verisi['TC Kimlik'].isin(kbs_verisi['TC Kimlik'])]
-ikys_in_kbs_out.to_excel('./rapor/' + str(bu_yil) + '/' + str(bu_ay) + '/ikysde_olup_kbsde_olmayanlar.xlsx', index=False, header=True, freeze_panes=(1,0))
-print('%70')
+		# İKS Sisteminde bilgisi bulunup, KBS Sisteminde kaydı bulunmayan Personel
+		ikys_in_kbs_out = ikys_verisi[~ikys_verisi['TC Kimlik'].isin(kbs_verisi['TC Kimlik'])]
+		ikys_in_kbs_out.to_excel('./rapor/' + str(bu_yil) + '/' + str(bu_ay) + '/ikysde_olup_kbsde_olmayanlar.xlsx', index=False, header=True, freeze_panes=(1,0))
+		print('%70')
 
-ikys_kbs_esitle = ikys_verisi.loc[ikys_verisi['TC Kimlik'].isin(kbs_verisi['TC Kimlik'])]
-ikys_kbs_esitle.to_excel('./rapor/' + str(bu_yil) + '/' + str(bu_ay) + '/ikys_personel_verileri.xlsx', index=False, header=True, freeze_panes=(1,0))
-print('%80')
+		ikys_kbs_esitle = ikys_verisi.loc[ikys_verisi['TC Kimlik'].isin(kbs_verisi['TC Kimlik'])]
+		ikys_kbs_esitle.to_excel('./rapor/' + str(bu_yil) + '/' + str(bu_ay) + '/ikys_personel_verileri.xlsx', index=False, header=True, freeze_panes=(1,0))
+		print('%80')
+	except:
+		print("Raporlama hatası....")
+
+if __name__ == '__main__':
+	raporla()
