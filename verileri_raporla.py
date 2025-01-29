f1efa22745ac9f86fd909a426b0888e962685f88
Yahya YILDIRIM <yahya.yldrm@gmail.com>
2025-01-30 00:13:10 +0300

    Üretilen raporların ismi değiştirildi.

--- verileri_raporla.py
+++ verileri_raporla.py
@@ -23,7 +23,7 @@ def raporla():
 
 		# KBS Sisteminde bilgisi bulunup, İKYS Sisteminde kaydı bulunmayan Personel
 		kbs_in_ikys_out = kbs_verisi[~kbs_verisi['TC Kimlik'].isin(ikys_verisi['TC Kimlik'])]
-		kbs_in_ikys_out.to_excel('./rapor/' + str(bu_yil) + '/' + str(bu_ay) + '/2-kbsde_olup_ikysde_olmayanlar.xlsx', index=False, header=True, freeze_panes=(1,0))
+		kbs_in_ikys_out.to_excel('./rapor/' + str(bu_yil) + '/' + str(bu_ay) + '/2-kbsde_olup_ikysde_personel.xlsx', index=False, header=True, freeze_panes=(1,0))
 		print('%50')
 
 		kbs_ikys_esitle = kbs_verisi.loc[kbs_verisi['TC Kimlik'].isin(ikys_verisi['TC Kimlik'])]
@@ -32,7 +32,7 @@ def raporla():
 
 		# İKS Sisteminde bilgisi bulunup, KBS Sisteminde kaydı bulunmayan Personel
 		ikys_in_kbs_out = ikys_verisi[~ikys_verisi['TC Kimlik'].isin(kbs_verisi['TC Kimlik'])]
-		ikys_in_kbs_out.to_excel('./rapor/' + str(bu_yil) + '/' + str(bu_ay) + '/3-ikysde_olup_kbsde_olmayanlar.xlsx', index=False, header=True, freeze_panes=(1,0))
+		ikys_in_kbs_out.to_excel('./rapor/' + str(bu_yil) + '/' + str(bu_ay) + '/3-ikysde_olup_kbsde_olmayan_personel.xlsx', index=False, header=True, freeze_panes=(1,0))
 		print('%70')
 
 		ikys_kbs_esitle = ikys_verisi.loc[ikys_verisi['TC Kimlik'].isin(kbs_verisi['TC Kimlik'])]
